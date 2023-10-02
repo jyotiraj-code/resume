6d4be878a44b4b5fe526a87e5772f02ea538c385
Jyotiraj Nath <jyotirajnath7@gmail.com>
2023-10-02 22:41:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // Add your form submission logic here, e.g., send data to a server or display a confirmation message.
+    });
+});
